ea4d6b953c3443c06d1aa2323cdb28acf4b0012e
Konstantin Novokshonov <konstantin.novok@gmail.com>
2020-01-12 21:35:12 +0300

    Implemented installation rbenv to $RBENV_DESTDIR

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+wget https://github.com/rbenv/rbenv/archive/master.tar.gz -O rbenv.tar.gz
+wget https://github.com/rbenv/ruby-build/archive/master.tar.gz -O ruby-build.tar.gz
+tar -xzf rbenv.tar.gz
+tar -xzf ruby-build.tar.gz
+
+CWD=$(pwd)/rbenv-master
+cd ${CWD}
+PREFIX=$CWD ../ruby-build-master/install.sh
+mv libexec/* bin
+
+if [ -z $RBENV_DESTDIR ]; then
+	RBENV_DESTDIR=/usr/local/rbenv
+	mkdir -p $RBENV_DESTDIR
+fi
+echo Installing rbenv to $RBENV_DESTDIR...
+mv bin completions rbenv.d share CONDUCT.md LICENSE README.md $RBENV_DESTDIR
+cd ..
+rm -rf rbenv-master ruby-build-master rbenv.tar.gz ruby-build.tar.gz
+echo Installed rbenv to $RBENV_DESTDIR
+echo You are need to add $RBENV_DESTDIR/bin directory to PATH variable
